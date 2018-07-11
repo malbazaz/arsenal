20caad7b70f95540ccfe713ff89190806b8cb3c1
malbazaz <39787850+malbazaz@users.noreply.github.com>
2018-07-11 19:38:18 +0200

    From the scraping: corrected the player's name by inversing the last, first name into first and last name.

--- lib/arsenal/scraper.rb
+++ lib/arsenal/scraper.rb
@@ -5,19 +5,19 @@ require 'pry'
 
 class Scraper
 
-binding.pry 
+#binding.pry 
 
 def self.scrape_main_page
 	# scrapes the main page and creates an array with hashes inside representing each player's name and url
 
     main_page = Nokogiri::HTML(open("https://www.arsenal.com/first-team/players"))
-    binding.pry 
+    #binding.pry 
     @scrapped_players = []
     first_http = "https://www.arsenal.com"
 #binding.pry
       main_page.css(".player-card").each_with_index do |player, index|
         @scrapped_players[index] = {
-          :name => player.css(".player-card__info__name").text + " " + player.css(".player-card__info__position-or-first-name").text,
+          :name => player.css(".player-card__info__position-or-first-name").text + " " + player.css(".player-card__info__name").text,
           :profile_url =>  first_http + player.css("a").attribute("href").value }
       end
      @scrapped_players
