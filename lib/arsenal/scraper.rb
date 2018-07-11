75dc94aeeacf57c1185365dcb02cac253ba8b37d
malbazaz <39787850+malbazaz@users.noreply.github.com>
2018-07-11 16:06:43 +0200

    finally found the solution on how to parse the values that I need. I commented out the one for Name for now. It was a struggle finding the sibling, and after more than an hour search online. Finally did it.What was key was to understand it is CSS code, not ruby or nokogiri specific. Anyway, done now. Time to code.

--- lib/arsenal/scraper.rb
+++ lib/arsenal/scraper.rb
@@ -5,9 +5,10 @@ require 'pry'
 
 class Scraper
 
-#binding.pry 
+binding.pry 
 
 def self.scrape_main_page
+
     main_page = Nokogiri::HTML(open("https://www.arsenal.com/first-team/players"))
     binding.pry 
     @scrapped_players = []
@@ -20,6 +21,29 @@ def self.scrape_main_page
           							}
       end
   end 
+
+
+def self.scrape_profile_page(player)
+	# takes in a player and provide his profile url
+    
+    profile_page = Nokogiri::HTML(open(profile_url))
+    profile_links={}
+      profile_page.css(".social-icon-container").children.css("a").each do |link|
+        if link.attribute("href").value.include?("twitter")
+          profile_links[:twitter] = link.attribute("href").value
+        elsif link.attribute("href").value.include?("linkedin")
+          profile_links[:linkedin] = link.attribute("href").value
+        elsif link.attribute("href").value.include?("github")
+          profile_links[:github] = link.attribute("href").value
+        else 
+          profile_links[:blog] = link.attribute("href").value
+        end 
+      end 
+    profile_links[:profile_quote] = profile_page.css(".profile-quote").text.strip
+    profile_links[:bio] = profile_page.css(".description-holder p").text.strip
+    profile_links
+  end
+#main_page.css(".info-card__key:contains('Name')~.info-card__value").first.text
 
 #binding.pry
 
